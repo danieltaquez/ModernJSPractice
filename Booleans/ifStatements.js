52565506b615ed6933a866c69a7feb1266bf7ff1
Daniel Taquez <daniel.taquez@du.edu>
2020-01-08 06:37:23 -0700

    if statments/ eles

---
+++ Booleans/ifStatements.js
@@ -0,0 +1,27 @@
+/*let temp = 33;
+let isFreezing = temp <= 31 ;
+
+if (isFreezing) {
+    console.log("it is cold outside")
+}*/
+
+//you can drop the varibeles right into the if statement and use the constatnt value.
+//let temp = 33;
+//if (temp <= 31) {
+    //console.log("it is cold outside")
+//}
+
+//if (temp > 31) {
+    //console.log("Your good bro")
+//};
+
+//challenge area
+let age = 23
+//if 7 or under discount
+if (age <= 7) {
+    console.log("Kid discount");
+} else if (age >= 65) {
+    console.log ("Old people discount");
+} else {
+    console.log("no");
+};
