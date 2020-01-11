8a90d534b47fecc0a02128a61400ac80a14521bc
Daniel Taquez <daniel.taquez@du.edu>
2020-01-11 12:11:44 -0700

    variable scope

---
+++ Booleans/booleansadvanced.js
@@ -0,0 +1,30 @@
+/*let isAccountLocked = false
+let userRole = 'dan'
+
+if (isAccountLocked) {
+    console.log ('Is account locked')
+} else {
+    console.log ('Welcome Dawg!')
+}
+
+if (isAccountLocked) {
+    console.log ('Is account locked')
+
+} else if (userRole === 'admin') {
+    console.log ('Welcome Dawg!')
+} else{
+    console.log ('Welcome boi')
+}
+*/
+
+let temp = 130;
+let freezingOutside = 31;
+let Hotoutside = 120;
+
+if (temp <= freezingOutside) {
+    console.log('ITs cold out')
+} else if (temp >= Hotoutside) {
+    console.log('Its a bit hot')
+} else {
+    console.log('Your good dawg, have a nice day')
+};
---
+++ Booleans/logic.js
@@ -0,0 +1,36 @@
+/* let temp = 65;
+if (temp >=60 && temp <=90) {
+    console.log('nice outside')
+} else {
+    console.log('dont go outside')
+}
+
+// coding with range
+// tru if boht sides are true &&/ two conditions being met
+//tur if only one is tru OR ||
+
+if (temp <= 0 || temp>= 120){
+    console.log('Dont go outside')
+}else {
+    console.log('Your good G')
+} */
+
+/* let temp = 0;
+if (temp >=60 && temp <=90) {
+    console.log('nice outside')
+} else if (temp <= 0 || temp>= 120){
+    console.log('Dont go outside')
+} else {
+    console.log ('up to you')
+} */
+
+let isguestOneVegan = true
+let isguestTwoVegan = false
+
+if (isguestOneVegan && isguestTwoVegan) {
+    console.log('Vegan menu');
+}else if  (isguestOneVegan || isguestTwoVegan){
+    console.log('Sugest vegan')
+}else {
+    console.log('Anything')
+}
