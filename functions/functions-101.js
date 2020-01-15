ba81fa4c6f4622bbda52dc1abaeb922108d6130b
Daniel Taquez <daniel.taquez@du.edu>
2020-01-14 20:34:33 -0700

    functions

---
+++ functions/functions-101.js
@@ -0,0 +1,38 @@
+// function - input, code. output
+
+
+/*let greetUser = function () {
+    console.log('Welcome User')
+}
+
+greetUser();
+
+//callback
+
+let square = function (num) {
+    let result = num * num
+    return result
+    
+}
+
+
+let value = square(3)
+let otherValue= square(10)
+
+console.log(value)
+console.log(otherValue)*/
+// chalange 
+//convert farenheighttocelsius
+
+
+
+let Far = function (num) {
+    let result = ((num -32)/1.8);
+    return result
+};
+
+let ConvertedCelsuis = Far(32)
+let otherValue = (68)
+
+console.log(ConvertedCelsuis)
+console.log(otherValue)
