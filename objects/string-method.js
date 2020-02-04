ef661fde29c5b039cc5718358d27047c3a772108
Daniel Taquez <daniel.taquez@du.edu>
2020-02-04 10:04:21 -0700

    string methods

---
+++ objects/string-method.js
@@ -0,0 +1,20 @@
+let name = 'Dan Taquez'
+
+
+//lenth propetry
+console.log(name.length)
+
+//convert to uper/lower case
+
+console.log(name.toUpperCase())
+
+console.log(name.toLowerCase())
+
+// includes boolean
+let password = 'abc1234sword'
+console.log(password.includes('password'))
+
+// trim meathod (goood for orginzing data)
+
+let string = ' wdw wddw wdw wdwkdwkd'
+console.log(string.trim())
